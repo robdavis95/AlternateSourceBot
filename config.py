5b1a68b72fb49df4f1f334cfa5c154ee83bd511a
Robert Davis <me@robertdavis.co.uk>
2019-01-01 14:57:37 +0000

    Adding a blank config file

---
+++ config.py
@@ -0,0 +1,36 @@
+#Config
+
+client_id = ""
+client_secret = ""
+user_agent = "Robert Davis ( /u/Houstons-Problem / http://robertdavis.co.uk )"
+# Reddit Username
+username = ""
+#Reddit Password
+password = ""
+# Google API key
+search_key = ""
+# Google Search engine
+search_engine = ""
+
+# Debug
+search_api_call = "https://www.googleapis.com/customsearch/v1?key=" + search_key + "&cx=" + search_engine + "&q=art"
+
+# == Notes == 
+# Alternate Sources 
+
+'''
+https://www.yahoo.com/news
+https://news.google.com/
+http://www.huffingtonpost.com/
+http://www.cnn.com/
+https://www.nytimes.com/
+http://www.nbcnews.com/
+https://www.washingtonpost.com/
+https://www.theguardian.com/
+https://www.wsj.com/
+http://abcnews.go.com/
+http://www.bbc.co.uk/news
+https://www.usatoday.com/
+http://www.latimes.com/
+http://www.nytimes.com/
+'''
